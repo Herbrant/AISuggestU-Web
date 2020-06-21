8551a90125899087b1512fad877c9daaf22d37e3
daxcpp <cdavide98carnemolla@gmail.com>
2020-06-22 01:29:58 +0200

    Revert to standard elastic client configuration

--- controllers/userRecommendation.go
+++ controllers/userRecommendation.go
@@ -31,14 +31,14 @@ func getRecommendation(user string) []ClassifiedRepo {
 	// Obtain a client and connect to the default Elasticsearch installation
 	// on 127.0.0.1:9200. Of course you can configure your client to connect
 	// to other hosts and configure it in various other ways.
-	client, err := elastic.NewClient(elastic.SetURL("elastic-search:9200"))
+	client, err := elastic.NewClient()
 	if err != nil {
 		// Handle error
 		panic(err)
 	}
 
 	// Ping the Elasticsearch server to get e.g. the version number
-	info, code, err := client.Ping("elastic-search:9200").Do(ctx)
+	info, code, err := client.Ping("http://127.0.0.1:9200").Do(ctx)
 	if err != nil {
 		// Handle error
 		panic(err)
@@ -46,7 +46,7 @@ func getRecommendation(user string) []ClassifiedRepo {
 	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
 
 	// Getting the ES version number is quite common, so there's a shortcut
-	esversion, err := client.ElasticsearchVersion("elastic-search:9200")
+	esversion, err := client.ElasticsearchVersion("http://127.0.0.1:9200")
 	if err != nil {
 		// Handle error
 		panic(err)
